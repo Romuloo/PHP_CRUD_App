bf68707a40a6894a8dec2791d61795f0e775d587
Javier Linares <j.linares5@usp.ceu.es>
2021-04-29 12:42:29 +0100

    Created class Users for the Log-in.

---
+++ app/controllers/Users.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Users{
+
+    public function __construct(){
+
+    }
+
+    public function register(){
+        // Check for POST
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Process form
+
+        } else{
+            // Init data
+            $data = [
+                'name' => '',
+                'email' => '',
+                'password' => '',
+                'confirm_password' => '',
+                'name_err' => '',
+                'email_err' => '',
+                'password_err' => '',
+                'confirm_password_err' => ''
+             ];
+
+            // Load view
+            $this -> view('users/register', $data);
+
+        }
+    }
+}
