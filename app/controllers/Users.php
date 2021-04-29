aac92314170b1f043547690360f3733efe3bd5db
Javier Linares <j.linares5@usp.ceu.es>
2021-04-29 13:35:56 +0100

    Login form validation done.

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -90,6 +90,37 @@ class Users extends Controller {
         // Check for POST
         if($_SERVER['REQUEST_METHOD'] == 'POST'){
             // Process form
+
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            // Init data
+            $data = [
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'email_err' => '',
+                'password_err' => ''
+            ];
+
+            // Validate Email
+            if(empty($data['email'])) {
+                $data['email_err'] = 'Please enter email';
+            }
+
+            // Validate Password
+            if(empty($data['password'])) {
+                $data['password_err'] = 'Please enter password';
+            }
+
+            // Make sure errors are empty
+            if(empty($data['email_err'])  && empty($data['password_err'])){
+                // Validated
+                die('SUCCESS');
+            } else{
+                // Load view with errors
+                $this -> view('users/login', $data);
+            }
+
         } else{
             // Init data
             $data = [
