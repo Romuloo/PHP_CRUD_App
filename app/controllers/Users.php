47496395850c2310cb2d6e721f4a9cad349fa11f
Javier Linares <j.linares5@usp.ceu.es>
2021-04-30 02:25:11 +0100

    Login method created.

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -129,10 +129,29 @@
                     $data['password_err'] = 'Please enter password';
                 }
 
+                // Check for user and email
+                if($this -> userModel -> findUserByEmail($data['email'])){
+                    // User found
+                }
+                else{
+                    // User not found
+                    $data['email_err'] = 'No user found';
+                }
+
                 // Make sure errors are empty
                 if(empty($data['email_err'])  && empty($data['password_err'])){
                     // Validated
-                    die('SUCCESS');
+                    // Check and set logged in user
+                    $loggedInUser = $this -> userModel -> login($data['email'], $data['password']);
+
+                    if($loggedInUser){
+                        // Create Session variables
+                        die('SUCCESS');
+                    } else { // Rerender the form with an error
+                        $data['password_err'] = 'Password incorrect';
+
+                        $this -> view('users/login' , $data);
+                    }
                 } else{
                     // Load view with errors
                     $this -> view('users/login', $data);
--- app/models/User.php
+++ app/models/User.php
@@ -22,6 +22,21 @@
             }
         }
 
+        // Login User
+        public function login($email, $password){
+            $this -> db -> query('SELECT * FROM users WHERE email = :email');
+            $this -> db -> bind(':email', $email);
+
+            $row = $this -> db -> single();
+
+            $hashed_password = $row -> password;
+            if(password_verify($password, $hashed_password)) {
+                return $row;
+            } else{
+                    false;
+                }
+        }
+
         // Find user by email
         public function findUserByEmail($email){
             // I use bind to prevent malicious code that can destroy my database.
